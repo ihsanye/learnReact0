03634603e234333b97e57e2b991d2875e019d519
ihsanye <ihsany98@gmail.com>
2024-02-04 22:58:08 +0300

    prop types

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 //import React from "react"
-import Header from './components/Header'
+//import Header from './components/Header'
 import User from './components/User'
 
 function App() {
   //return React.createElement("div", null, "Hello");
   return (
     <div className="xyz">
-      <User name="ihs" last="yeni" isLoggedIn={true} age={25} friends={["sefa", "berkay", "ihsan"]} />
+      <User name="ihs" last="yeni" isLoggedIn={true} age={25} friends={["sefa", "berkay", "ihsan"]} address={{ title: "Ankara", zip: "06830" }} />
     </div>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,20 +1,14 @@
-// function User(props) {
-//     console.log(props);
-//     return (
-//         <div><h2>{
-//             props.isLoggedIn ? `Hosgeldin ${props.name} ${props.last}` : "Gecersiz Islem"
-//         }</h2>
-//         </div>
-//     )
-// }
-// export default User;
+import PropTypes from "prop-types";
 
-function User({ name, last, isLoggedIn, age, friends }) {
+function User({ name, last, isLoggedIn, age, friends, address }) {
     return (
         <div>
             <h1>
                 {isLoggedIn ? `Hosgeldin ${name} ${age}` : "Gecersiz Islem"}
             </h1>
+            {address.title} {address.zip}
+            <br />
+            <br />
             {friends.map((friend, index) => (
                 <div key={index}>
                     {index} - {friend}
@@ -22,4 +16,25 @@ function User({ name, last, isLoggedIn, age, friends }) {
         </div>
     )
 }
+
+User.propTypes = {
+    name: PropTypes.string.isRequired,
+    last: PropTypes.string.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    age: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string]),
+    friends: PropTypes.array,
+    address: PropTypes.shape({
+        title: PropTypes.string,
+        zip: PropTypes.string,
+    })
+};
+//zip number olrsa 0 ile baslayan sayilar error veriyor, parseInt ile cozulur
+
+User.defaultProps = {
+    name: "isimsiz",
+    isLoggedIn: false
+};
+//deger gonderilmemisse varsayilan olarak bunlar gonderilir
 export default User;
