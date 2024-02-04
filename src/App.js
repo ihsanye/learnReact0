6f36b31990095d40cd3a40d835764659ba3aa70a
ihsanye <ihsany98@gmail.com>
2024-02-04 21:25:37 +0300

    input ve kosul

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 import Header from './components/Header'
 
 const name = "ihs";
+const last = "yeni";
+const isLoggedIn = false;
 
 function App() {
   //return React.createElement("div", null, "Hello");
   return (
     <div className="xyz">
-      <h1>Hello React</h1>
-      <Header />
-      <label htmlFor="name">
-        Name:
-        <input id="name" type="text"></input>
-      </label>
+
+      <h1>{name} {last}</h1>
+      <h2>{isLoggedIn && `Merhaba ben ${name + " " + last}`}</h2>
+      <h2>{!isLoggedIn && "Giris Yapmadiniz."}</h2>
+      <h3>
+        {isLoggedIn ? `Hosgeldin ${name + last}` : "Islem Basarisiz"}
+      </h3>
     </div>
 
   );
