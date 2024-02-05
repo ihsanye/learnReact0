7de546459ba822b46cdbaa7fdc9c22717e4cd6c9
ihsanye <ihsany98@gmail.com>
2024-02-05 15:33:49 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0);
   const [name, setName] = useState("ihs");
-  const [age, setAge] = useState(25);
-  const [friends, setFriends] = useState(['ihs', 'sfa', 'brky']);
-  const [address, setAddress] = useState({ city: "Ankara", zip: 6830 });
-  console.log(name, age);
-  //herhangi bir state guncellendigi anda alttaki islem(render) bastan yapilir, dolayisiyla deger guncellenir
 
+  useEffect(() => {
+    console.log("component mount edildi");
+  }, []);
+
+  useEffect(() => {
+    console.log("number state guncellendi");
+  }, [number]);
+  //array bos birakilirsa tum degisimleri; number,name yazilirsa her ikisini kontrol eder.
+  //ayri ayri useEffect ler kullanilirsa ayri ayri kontrol eder
   return (
     <div className="App">
-      <h1>Merhaba! {name} {age}</h1>
-      <button onClick={() => setName('sefa')}>Change Name</button>
-      <button onClick={() => setAge(18)}>Change Age</button>
-      <hr />
-      <h3>Friends</h3>
-      {
-        friends.map((friend, index) => (
-          <div key={index}>{friend}</div>
-        ))
-      }
-      <button onClick={() => setFriends([...friends, "yeni"])}>Add friend</button>
-
-      <h3>Address</h3>
-      <div>{address.city} {address.zip} </div>
-      <button onClick={() => setAddress({ ...address, zip: 6999 })}>Address update</button>
-
-      {/* degistirmek yerine eklemek icin objectleri array haline getirmemiz gerek "[{ }]" seklinde */}
-
+      <h2>{number}</h2>
+      <button onClick={() => setNumber(number + 1)} >Up</button>
+      <br />
+      <h2>{name}</h2>
+      <button onClick={() => setName("brky")} >Change</button>
     </div>
   );
 }
