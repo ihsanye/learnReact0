e040f4db3b6993b1a24df7d47b69dcc7259ffaff
ihsanye <ihsany98@gmail.com>
2024-02-05 13:44:58 +0300

    states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
-//import React from "react"
-//import Header from './components/Header'
-import User from './components/User'
+import { useState } from 'react';
 
 function App() {
-  //return React.createElement("div", null, "Hello");
+  const [name, setName] = useState("ihs");
+  const [age, setAge] = useState(25);
+  const [friends, setFriends] = useState(['ihs', 'sfa', 'brky']);
+  const [address, setAddress] = useState({ city: "Ankara", zip: 6830 });
+  console.log(name, age);
+  //herhangi bir state guncellendigi anda alttaki islem(render) bastan yapilir, dolayisiyla deger guncellenir
+
   return (
-    <div className="xyz">
-      <User name="ihs" last="yeni" isLoggedIn={true} age={25} friends={["sefa", "berkay", "ihsan"]} address={{ title: "Ankara", zip: "06830" }} />
+    <div className="App">
+      <h1>Merhaba! {name} {age}</h1>
+      <button onClick={() => setName('sefa')}>Change Name</button>
+      <button onClick={() => setAge(18)}>Change Age</button>
+      <hr />
+      <h3>Friends</h3>
+      {
+        friends.map((friend, index) => (
+          <div key={index}>{friend}</div>
+        ))
+      }
+      <button onClick={() => setFriends([...friends, "yeni"])}>Add friend</button>
+
+      <h3>Address</h3>
+      <div>{address.city} {address.zip} </div>
+      <button onClick={() => setAddress({ ...address, zip: 6999 })}>Address update</button>
+
+      {/* degistirmek yerine eklemek icin objectleri array haline getirmemiz gerek "[{ }]" seklinde */}
+
     </div>
   );
 }
